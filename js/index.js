02a713752eaa3f38f099b76701d3539c571c04a6
MJ-bot-cruise <139448750+MJ-bot-cruise@users.noreply.github.com>
2024-02-16 01:43:18 +0700

    first sync

--- js/index.js
+++ js/index.js
@@ -11,8 +11,10 @@ function convert() {
         result = temptInput *  1.8 + 32;
         out = result.toFixed(2) + " ℉";
     } else {
+        if ( unit === "fahrenheit") {
         result = (temptInput - 32) / 1.8 ;
-        out = hasil.toFixed(2) + " ℃";
+        out = out.toFixed(2) + " ℃";
+        }
     }
     document.getElementById("out").innerHTML = out;
     document.getElementById("out").classList.remove("error");
